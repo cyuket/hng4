cc09b92c0c35f5b10e96dd63a7aecf4b901689cb
syziddy65 <cyriluket12@gmail.com>
2018-04-09 14:16:28 +0100

    my stage two work

---
+++ index.php
@@ -0,0 +1,22 @@
+<doctype html>
+ <html>
+ 	<head>
+ 		<title> hnginternship4 </title>
+ 		<meta charset="utf-8"/>
+ 		<link rel="stylesheet" /css" href="style.css"/>
+ 	</head>
+ 	<body>
+ 		<br><br>
+ 		<h3> HELLO EVERYONE </h3>
+ 		<br><br><br>
+ 		<h3> I REALLY LOVE TO BE HERE </h3>
+ 		<br><br><br><br><br><br>
+ 		<h3>THIS IS MY FIRST TASK </h3> 
+ 		<br><br><br><br><br>
+ 		<?php
+ 		$time = time();
+ 		$realTime = date('F jS Y h: i a'); 
+ 		 ?>
+ 		 <h3>TIME: <?php echo $realTime; ?> </h3>
+ 	</body>
+ </html>
